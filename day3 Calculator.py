9dc5383ffafc8331475fe32929d7845100ed45f7
yashkumar-ui <77090537+yashkumar-ui@users.noreply.github.com>
2021-05-07 10:39:35 +0530

    Add files via upload

---
+++ day3 Calculator.py
@@ -0,0 +1,18 @@
+# code of simple calculator
+
+m=int(input('Enter 1st number = '))
+
+n=int(input("Enter 2nd number = "))
+
+
+print('the addition of', m ,'and',n, 'is ','=',   m+n)
+
+print('the subtraction of', m ,'and',n, 'is ','=',m-n)
+
+print('the division of', m ,'and ',n, 'is ','=',  m/n, 'and remainder will be =',m%n)
+
+print('the multiplication of', m ,'and ',n, 'is ','=',m*n)
+
+print('the double division(//) of', m ,'and',n, 'is','=',m//n ,'and remainder will be = ',m%n)
+
+print('the',m,'raise to power of',n,'is ','=',m**n) 
